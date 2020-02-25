887cc82208284c78eb83f06aa354310550597612
Kendall Jackson <kendallkj00@gmail.com>
2020-02-25 17:31:56 -0500

    Fixed transparent navbar on ios

--- src/components/CustomFileInput.vue
+++ src/components/CustomFileInput.vue
@@ -10,7 +10,7 @@
                 d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"
               /></svg></p></div>
         </section>
-        <input @change="changeSweg" type="file" name="File(s)" class="hidden">
+        <input @change="changeSweg" type="file" name="File(s)" class="hidden" accept="image/x-png, image/png">
       </label>
     </div>
     <FileLoader v-show="uploading" />
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -7,7 +7,7 @@
 <style>
 @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,600,700,900&display=swap');
 body {
-  @apply bg-green-700;
+  background-color: #2F855A !important;
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='4' height='4' viewBox='0 0 4 4'%3E%3Cpath fill='%231a422e' fill-opacity='0.25' d='M1 3h1v1H1V3zm2-2h1v1H3V1z'%3E%3C/path%3E%3C/svg%3E");
 }
 html {
@@ -19,7 +19,7 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
-  background-color: #f1f1f1;
+  background-color: #2F855A !important;
 }
 
 *,
